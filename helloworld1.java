2400901bdd9734ad1a5d708e480586c35cdf6363
roopagowda <roopagowda2294@gmail.com>
2020-02-22 11:03:10 +0530

    Create helloworld1.java

---
+++ helloworld1.java
@@ -0,0 +1,17 @@
+public class helloworld1
+{
+// main method begins execution of Java application
+public static void main( String[] args )
+{
+System.out.println("Hello World!");
+System.out.println("Welcome to Java Programming! on Ubuntu 10.04");
+
+
+System.out.println("Checking for second commit");
+System.out.println("Checking for third commit");
+System.out.println("Checking for fourth commit");
+System.out.println("Checking for fifth commit");
+
+
+} // method main ends here
+} // end of the class Welcome
